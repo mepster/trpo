55be86323399a8b4871028a24ba815f2024dd49c
Michael E. Palmer <mep@mepalmer.net>
2017-10-28 03:35:30 +0800

    first checkin of trpo-mep. train2.py works with mujoco. want to put osim-train2 on linux to try.

---
+++ src/mpi_util.py
@@ -0,0 +1,332 @@
+from mpi4py import MPI
+import tensorflow as tf
+import os, subprocess, sys
+import numpy as np
+import random
+import time
+
+"""
+
+                                Parallelizing RL batch algorithms
+                                        Gary McIntire
+
+    There are multiple ways to do this, but the easiest one(shown here) is to have all processes do their rollouts and then gather
+    those rollouts back to process 0. Let process 0 compute the weight updates, then broadcast those weight updates
+    from rank0 to all the other processes.
+
+    Another example where batches are 'not' accumulated to rank 0 is the trpo_mpi code of the openai baselines
+    https://github.com/openai/baselines/tree/master/baselines/trpo_mpi  where the gradients are averaged together
+    That method can require more knowledge of the actual algo whereas accumlating batches to rank0 works for
+    almost all batch RL algorithms
+
+0. Install mpi4ph
+    conda install mpi4ch mpich2
+
+1. Make the ConfigProto use as small an amount of memory as possible. Add gpu_options like this
+    self.sess = tf.Session(graph=self.g, config=mpi_util.tf_config)  # see tf_config in the code below
+
+2. Add mpi_util
+	import mpi_util
+
+3. When nprocs is known shortly after program start, fork nprocs...
+	if "parent" == mpi_util.mpi_fork(args.nprocs, gpu_pct=args.gpu_pct): sys.exit()
+
+4. Each process and environment will need different random seeds computed from
+    mpi_util.set_global_seeds(seed+mpi_util.rank)
+    env.seed(seed+mpi_util.rank)
+
+5. print statements, env monitoring/rendering, etc may need to be conditional such that only one process does it
+    if mpi_util.rank == 0: print(...
+    it can be useful to prepend print statements with the rank   print( str(mpi_util.rank) + ... )
+
+6. Accumulate the batches with something like
+    d = mpi_util.rank0_accum_batches({'advantages': advantages, 'actions': actions, 'observes': observes, 'disc_sum_rew': disc_sum_rew})
+    observes, actions, disc_sum_rew, advantages = d['observes'], d['actions'], d['disc_sum_rew'], d['advantages']
+
+7. Since this accumulates the batches to rank0, you can avoid processing weight updates on the other processes
+    if mpi_util.rank==0:
+        policy.update(observes, actions, advantages, logger)  # update policy
+        val_func.fit(observes, disc_sum_rew, logger)  # update value function
+
+8. After updating the weights on rank0, broadcast the weights from rank0 back to all the other processes
+    mpi_util.rank0_bcast_wts(val_func.sess, val_func.g, 'val')
+    mpi_util.rank0_bcast_wts(policy.sess, policy.g, 'policy')
+
+9.  You should be able to use multiple gpu cards if you have them (untested)
+    with tf.device('/gpu:'+str(mpi_util.rank % numGPUcards)):
+        main()
+
+10. You should be able to use multiple computers as well. See mpirun documentation on host files
+
+
+    This code is for tensorflow, but a few alterations would allow it to work on theano, pytorch, etc
+
+    The actual speedup from parallelizing is most dependent on the batch size. Larger batch sizes will get a closer to
+    linear speedup, but even small batch sizes can triple or quadruple your wall clock speed with nprocs = 8
+
+
+                        Timing  -  on single Xeon 5680 Hex core/twelve thread  GTX 1070
+
+    python ../train.py Walker2d-v1 --nprocs 10 --gpu_pct 0.05  -n 2000
+
+nprocs  steps_per_sec   reward_mean
+1            546             641        # reward is highly variable because robot is highly variable
+2           1040             544
+3           1712             611
+4           2014            1220
+5           1950            1513
+6           2100             860
+7           2300             681
+8           2400             452
+9           2484             359
+10          2339             367
+11          2384             410
+12          2169             336
+"""
+
+
+tf_config = None
+
+nworkers=1      # defaults for a single process
+rank=0
+
+def mpi_fork(n, gpu_pct=0.0):
+    """Re-launches the current script with workers
+    Returns "parent" for original parent, "child" for MPI children
+    """
+    global tf_config
+    if gpu_pct > 0:
+        tf_config = tf.ConfigProto(
+            gpu_options =tf.GPUOptions(
+                                per_process_gpu_memory_fraction=gpu_pct,
+                                # allow_growth=True
+                                ),
+            inter_op_parallelism_threads=1,
+            intra_op_parallelism_threads=1)
+    else:
+        tf_config = tf.ConfigProto(
+            gpu_options =tf.GPUOptions( allow_growth=True),
+            inter_op_parallelism_threads=1,
+            intra_op_parallelism_threads=1)
+    if n<=1:
+        return "child"
+    if os.getenv("IN_MPI") is None:
+        env = os.environ.copy()
+        env.update(
+            MKL_NUM_THREADS="1",
+            OMP_NUM_THREADS="1",
+            IN_MPI="1"
+        )
+        cmd = ["mpirun", "-np", str(n),   sys.executable] +['-u']+ sys.argv
+        print(cmd)
+        subprocess.check_call(cmd, env=env)
+        # subprocess.check_call(["/usr/bin/mpirun", "-np", str(n), '-mca', 'coll_tuned_bcast_algorithm', '0', sys.executable] +['-u']+ sys.argv, env=env)       # this mpirun is 1/3 the speed of the one above
+        return "parent"
+    else:
+        global nworkers, rank
+        nworkers = MPI.COMM_WORLD.Get_size()
+        rank = MPI.COMM_WORLD.Get_rank()
+        # print('assigning the rank and nworkers', nworkers, rank)
+        return "child"
+
+def broadcast_policy_scaler_val(policy, scaler, val_func):
+    rank0_bcast_wts(policy.sess, policy.g, 'policy')
+    #mpi_util.chksum(policy.sess, policy.g, 'policy')
+    rank0_bcast_wts(val_func.sess, val_func.g, 'val_func')
+    #mpi_util.chksum(val_func.sess, val_func.g, 'val_func')
+    MPI.COMM_WORLD.Bcast(scaler.vars, root=0)
+    MPI.COMM_WORLD.Bcast(scaler.means, root=0)
+    scaler.m = MPI.COMM_WORLD.bcast(scaler.m, root=0)
+    scaler.n = MPI.COMM_WORLD.bcast(scaler.n, root=0)
+    # print(scaler.vars)
+    # print(scaler.means)
+    # print(scaler.m)
+    # print(scaler.n)
+    return policy, scaler, val_func
+
+def gather_trajectories(trajectories):
+    return MPI.COMM_WORLD.gather(trajectories, root=0)
+
+def set_global_seeds(i):
+    try:
+        import tensorflow as tf
+    except ImportError:
+        pass
+    else:
+        tf.set_random_seed(i)
+    np.random.seed(i)
+    random.seed(i)
+
+def accum_batches(segs):
+    combo = dict()
+    for k in segs[0].keys():
+        if type( segs[0][k ]) is list:
+            # print(k,'is list')
+            combo[k] = sum([segs[i][k] for i in range(len(segs))], [])
+        elif type( segs[0][k ]) is np.float64:
+            # print(k,'is float64')
+            pass    # ignore nextvpred as it is not used in further code
+        else:   # must be a numpy.ndarray
+            # print(k,type(segs[0][k ]))
+            combo[k] = np.concatenate([segs[i][k] for i in range(len(segs))])
+    pass
+    return combo
+
+def print0(string):
+    if rank == 0:
+        print(string)
+
+cumsum_steps=0
+tstart = time.time()
+def steps_sec(stepcnt):
+    global tstart, cumsum_steps
+    cumsum_steps += stepcnt
+    # print0('cumsum_steps=%d' % cumsum_steps)   # only for step counting
+    etime = time.time()
+    # print0('%d_steps_sec=%d' % (rank, stepcnt/(etime-tstart)))
+    tstart = etime
+
+def mpitype(typ):
+    if typ == np.float64:
+        return MPI.DOUBLE
+    elif typ == np.float32:
+        return MPI.FLOAT
+    else: return None
+
+def rank0_accum_batches(segins):
+    if nworkers == 1: return segins   # don't bother if only one process
+    combo = dict()
+    for k in sorted(segins):
+        seq = segins[k]
+        recbuf = None
+        counts = MPI.COMM_WORLD.gather(seq.shape[0], root=0)
+        if rank==0: Nrows = sum(counts)
+        if rank==0:
+            if len(seq.shape) == 2:
+                recbuf = np.zeros((Nrows, seq.shape[-1]),dtype=seq.dtype)
+            else:
+                recbuf = np.zeros(Nrows,dtype=seq.dtype)
+        if len(seq.shape) == 2:
+            rowtype = mpitype(seq.dtype).Create_contiguous(seq.shape[-1])
+        else:
+            rowtype = mpitype(seq.dtype)
+        rowtype.Commit()
+        # print(str(rank)+'rank key',k,seq.shape, recbuf.shape if rank==0 else -1111)
+        MPI.COMM_WORLD.Gatherv(sendbuf=[seq, mpitype(seq.dtype)] , recvbuf=[recbuf, (counts, None), rowtype], root=0)
+        if rank==0: combo[k] = recbuf[:Nrows]
+        pass
+    if rank==0: return combo
+    else: return segins
+
+def all_accum_batches(segs):
+    segs = MPI.COMM_WORLD.allgather(segs)
+    return accum_batches(segs)
+
+def all_sum(x):
+    if isinstance(x, np.ndarray):
+        out = np.empty_like(x)
+        MPI.COMM_WORLD.Allreduce(x, out, op=MPI.SUM)
+        return out
+    else:           # if not a numpy array, try it as a scalar
+        return all_sum(np.array([x]))[0]
+
+def all_mean(x):
+    return all_sum(x) / nworkers
+
+def bcast(x):
+    # print(str(rank)+'bcast sees',type(x),x.shape)
+    origshape = x.shape
+    if 1 or len(x.shape)==1:
+        # print(str(rank)+'if True')
+        MPI.COMM_WORLD.Bcast(x, root=0)
+    else:
+        print(str(rank)+'if False   prod',np.prod(x.shape))
+        inv = np.reshape(x, (np.prod(x.shape)))
+        print(str(rank)+'inv',type(inv),inv.shape,inv.dtype)
+        MPI.COMM_WORLD.Bcast(inv, root=0)
+        print(str(rank)+'outv',type(inv))
+        outv = inv.reshape(origshape)
+        print(str(rank)+'outv after reshape',type(outv),outv.shape)
+    return x
+
+def pt_to_pt_bcast(x):
+    for i in range(1,nworkers):
+        if rank == 0:
+            MPI.COMM_WORLD.Send(x, dest=i)
+        elif rank == i:
+            MPI.COMM_WORLD.Recv(x, source=0)
+    return x
+
+cache = {}
+def set_wt(sess, graph, var, val):
+    k = str(var)
+    kc = str(sess)+str(graph)+str(var)
+    if not kc in cache:
+        with graph.as_default():
+            ph = tf.placeholder(val.dtype, shape=val.shape)
+            op = var.assign(ph)
+            cache[kc] = [ var, ph, op]
+    _, ph, op = cache[kc]
+    sess.run(op, feed_dict={ph: val})
+
+def rank0_bcast_wts(sess, graph, label=''):
+    if nworkers == 1: return    # don't bother if only one process
+    # chksum(sess, graph, label=label+'before'+str(rank))
+    for var in sorted(graph.get_collection('trainable_variables'),key=lambda x: x.name):
+        # timeit(label+'______'+str(var))
+        w = sess.run(var)
+        # timeit(label+'sess.run'+str(var))
+        wts = bcast(w)
+        # print(str(rank)+'result type',type(wts))
+        # timeit(label+'bcast'+str(var))
+        if rank != 0:
+            # print(label+'set_wts with type',type(wts), wts.shape, str(wts.__array_interface__['data']),var)
+            set_wt(sess, graph, var, wts)
+            pass
+        # timeit(label+'set_wt'+str(var))
+    # chksum(sess, graph, label=label+'after'+str(rank))
+    # print('rank:',rank, 'nworkers',nworkers, 'label',label,'chksum', chksum)
+
+def all_avg_wts(sess, graph, label=''):
+    if nworkers == 1: return    # don't bother if only one process
+    # for var in graph.get_collection('trainable_variables'):
+    # print('label in all_avg_wts',label)
+    chksum = 0
+    for var in sorted(graph.get_collection('trainable_variables'),key=lambda x: x.name):
+        # print('rank:',rank,'label',label,'var',var)
+        # tf.assign(var, all_mean(sess.run(var)))    suspected memory leaker
+        wts = all_mean(sess.run(var))
+        # chksum += wts.sum()
+        set_wt(sess, graph, var, wts)
+        # sess.run(var, feed_dict={var: wts})
+    # print('rank:',rank, 'nworkers',nworkers, 'all_avg_wts label',label,'chksum', chksum)
+
+def chksum(sess, graph, label=''):
+    # if nworkers == 1: return    # don't bother if only one process
+    # for var in graph.get_collection('trainable_variables'):
+    print('label in chksum',label)
+    chksum = 0
+    for var in sorted(graph.get_collection('trainable_variables'),key=lambda x: x.name):
+        # print('rank:',rank,'label',label,'var',var)
+        # tf.assign(var, all_mean(sess.run(var)))    suspected memory leaker
+        wts = sess.run(var)
+        chksum += wts.sum()
+        sess.run(var, feed_dict={var: wts})
+    print('CHKSUM rank:',rank, 'nworkers',nworkers, 'label',label,'chksum', chksum)
+
+loop_start_time = -1
+lasttime = 0.
+def timeit(label):
+    if rank!=0: return
+    global lasttime, loop_start_time
+    thistime = time.time()
+    if '------' in label:    # bunch of hyphens indicates top of the loop
+        if loop_start_time > 0:
+            print(str(rank)+'timed-'+label+' TimeofLastLoop',thistime-loop_start_time,label)    # print the time of the last loop
+        loop_start_time = thistime
+        lasttime = thistime
+    else:
+        print(str(rank)+'timed-'+label,thistime-lasttime)
+    lasttime = thistime
+
+
---
+++ src/osim-train2.py
@@ -0,0 +1,365 @@
+#! /usr/bin/env python3
+"""
+PPO: Proximal Policy Optimization
+
+Written by Patrick Coady (pat-coady.github.io)
+
+PPO uses a loss function and gradient descent to approximate
+Trust Region Policy Optimization (TRPO). See these papers for
+details:
+
+TRPO / PPO:
+https://arxiv.org/pdf/1502.05477.pdf (Schulman et al., 2016)
+
+Distributed PPO:
+https://arxiv.org/abs/1707.02286 (Heess et al., 2017)
+
+Generalized Advantage Estimation:
+https://arxiv.org/pdf/1506.02438.pdf
+
+And, also, this GitHub repo which was helpful to me during
+implementation:
+https://github.com/joschu/modular_rl
+
+This implementation learns policies for continuous environments
+in the OpenAI Gym (https://gym.openai.com/). Testing was focused on
+the MuJoCo control tasks.
+"""
+import opensim as osim
+import numpy as np
+
+from osim.env import *
+from osim.http.client import Client
+from keras.optimizers import RMSprop
+import math
+
+from policy import Policy
+from value_function import NNValueFunction
+import scipy.signal
+from utils import Logger, Scaler
+from datetime import datetime
+import os
+import argparse
+import signal
+import itertools
+
+import mpi_util
+
+class GracefulKiller:
+    """ Gracefully exit program on CTRL-C """
+    def __init__(self):
+        self.kill_now = False
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
+
+
+def init_osim():
+    env = RunEnv(False)
+    obs_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
+
+    return env, obs_dim, act_dim
+
+def run_episode(env, policy, scaler, animate=False):
+    """ Run single episode with option to animate
+
+    Args:
+        env: ai gym environment
+        policy: policy object with sample() method
+        scaler: scaler object, used to scale/offset each observation dimension
+            to a similar range
+        animate: boolean, True uses env.render() method to animate episode
+
+    Returns: 4-tuple of NumPy arrays
+        observes: shape = (episode len, obs_dim)
+        actions: shape = (episode len, act_dim)
+        rewards: shape = (episode len,)
+        unscaled_obs: useful for training scaler, shape = (episode len, obs_dim)
+    """
+    obs = np.array(env.reset())
+    observes, actions, rewards, unscaled_obs = [], [], [], []
+    done = False
+    step = 0.0
+    scale, offset = scaler.get()
+    scale[-1] = 1.0  # don't scale time step feature
+    offset[-1] = 0.0  # don't offset time step feature
+    while not done:
+        if animate:
+            env.render()
+        obs = obs.astype(np.float32).reshape((1, -1))
+        obs = np.append(obs, [[step]], axis=1)  # add time step feature
+        unscaled_obs.append(obs)
+        obs = (obs - offset) * scale  # center and scale observations
+        observes.append(obs)
+        action = policy.sample(obs).reshape((1, -1)).astype(np.float32)
+        actions.append(action)
+        obs, reward, done, _ = env.step(np.squeeze(action, axis=0))
+        obs = np.array(obs)
+        if not isinstance(reward, float):
+            reward = np.asscalar(reward)
+        rewards.append(reward)
+        step += 1e-3  # increment time step feature
+
+    return (np.concatenate(observes), np.concatenate(actions),
+            np.array(rewards, dtype=np.float64), np.concatenate(unscaled_obs))
+
+
+def run_policy(env, policy, scaler, episodes):
+    """ Run policy and collect data for a minimum of min_steps and min_episodes
+
+    Args:
+        env: ai gym environment
+        policy: policy object with sample() method
+        scaler: scaler object, used to scale/offset each observation dimension
+            to a similar range
+        episodes: total episodes to run
+
+    Returns: list of trajectory dictionaries, list length = number of episodes
+        'observes' : NumPy array of states from episode
+        'actions' : NumPy array of actions from episode
+        'rewards' : NumPy array of (un-discounted) rewards from episode
+        'unscaled_obs' : NumPy array of (un-discounted) rewards from episode
+    """
+    trajectories = []
+    for e in range(episodes):
+        observes, actions, rewards, unscaled_obs = run_episode(env, policy, scaler)
+        # print "episode:", e, " of:", episodes, " done." #, rewards
+        print(".", end="")
+        trajectory = {'observes': observes,
+                      'actions': actions,
+                      'rewards': rewards,
+                      'unscaled_obs': unscaled_obs}
+        trajectories.append(trajectory)
+
+    return trajectories
+
+
+def discount(x, gamma):
+    """ Calculate discounted forward sum of a sequence at each point """
+    return scipy.signal.lfilter([1.0], [1.0, -gamma], x[::-1])[::-1]
+
+
+def add_disc_sum_rew(trajectories, gamma):
+    """ Adds discounted sum of rewards to all time steps of all trajectories
+
+    Args:
+        trajectories: as returned by run_policy()
+        gamma: discount
+
+    Returns:
+        None (mutates trajectories dictionary to add 'disc_sum_rew')
+    """
+    for trajectory in trajectories:
+        if gamma < 0.999:  # don't scale for gamma ~= 1
+            rewards = trajectory['rewards'] * (1 - gamma)
+        else:
+            rewards = trajectory['rewards']
+        disc_sum_rew = discount(rewards, gamma)
+        trajectory['disc_sum_rew'] = disc_sum_rew
+
+
+def add_value(trajectories, val_func):
+    """ Adds estimated value to all time steps of all trajectories
+
+    Args:
+        trajectories: as returned by run_policy()
+        val_func: object with predict() method, takes observations
+            and returns predicted state value
+
+    Returns:
+        None (mutates trajectories dictionary to add 'values')
+    """
+    for trajectory in trajectories:
+        observes = trajectory['observes']
+        values = val_func.predict(observes)
+        trajectory['values'] = values
+
+
+def add_gae(trajectories, gamma, lam):
+    """ Add generalized advantage estimator.
+    https://arxiv.org/pdf/1506.02438.pdf
+
+    Args:
+        trajectories: as returned by run_policy(), must include 'values'
+            key from add_value().
+        gamma: reward discount
+        lam: lambda (see paper).
+            lam=0 : use TD residuals
+            lam=1 : A =  Sum Discounted Rewards - V_hat(s)
+
+    Returns:
+        None (mutates trajectories dictionary to add 'advantages')
+    """
+    for trajectory in trajectories:
+        if gamma < 0.999:  # don't scale for gamma ~= 1
+            rewards = trajectory['rewards'] * (1 - gamma)
+        else:
+            rewards = trajectory['rewards']
+        values = trajectory['values']
+        # temporal differences
+        tds = rewards - values + np.append(values[1:] * gamma, 0)
+        advantages = discount(tds, gamma * lam)
+        trajectory['advantages'] = advantages
+
+
+def build_train_set(trajectories):
+    """
+
+    Args:
+        trajectories: trajectories after processing by add_disc_sum_rew(),
+            add_value(), and add_gae()
+
+    Returns: 4-tuple of NumPy arrays
+        observes: shape = (N, obs_dim)
+        actions: shape = (N, act_dim)
+        advantages: shape = (N,)
+        disc_sum_rew: shape = (N,)
+    """
+    observes = np.concatenate([t['observes'] for t in trajectories])
+    actions = np.concatenate([t['actions'] for t in trajectories])
+    disc_sum_rew = np.concatenate([t['disc_sum_rew'] for t in trajectories])
+    advantages = np.concatenate([t['advantages'] for t in trajectories])
+    # normalize advantages
+    advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-6)
+
+    return observes, actions, advantages, disc_sum_rew
+
+
+def log_batch_stats(observes, actions, advantages, disc_sum_rew, logger, episode):
+    """ Log various batch statistics """
+    logger.log({'_mean_obs': np.mean(observes),
+                '_min_obs': np.min(observes),
+                '_max_obs': np.max(observes),
+                '_std_obs': np.mean(np.var(observes, axis=0)),
+                '_mean_act': np.mean(actions),
+                '_min_act': np.min(actions),
+                '_max_act': np.max(actions),
+                '_std_act': np.mean(np.var(actions, axis=0)),
+                '_mean_adv': np.mean(advantages),
+                '_min_adv': np.min(advantages),
+                '_max_adv': np.max(advantages),
+                '_std_adv': np.var(advantages),
+                '_mean_discrew': np.mean(disc_sum_rew),
+                '_min_discrew': np.min(disc_sum_rew),
+                '_max_discrew': np.max(disc_sum_rew),
+                '_std_discrew': np.var(disc_sum_rew),
+                '_Episode': episode
+                })
+
+
+def main(env_name, num_episodes, gamma, lam, kl_targ, batch_size, nprocs, policy_hid_list, valfunc_hid_list, gpu_pct):
+    """ Main training loop
+
+    Args:
+        env_name: OpenAI Gym environment name, e.g. 'Hopper-v1'
+        num_episodes: maximum number of episodes to run
+        gamma: reward discount factor (float)
+        lam: lambda from Generalized Advantage Estimate
+        kl_targ: D_KL target for policy update [D_KL(pi_old || pi_new)
+        batch_size: number of episodes per policy training batch
+    """
+    # killer = GracefulKiller()
+
+    env, obs_dim, act_dim = init_osim()
+    env.seed(111 + mpi_util.rank)
+    mpi_util.set_global_seeds(111 + mpi_util.rank)
+
+    obs_dim += 1  # add 1 to obs dimension for time step feature (see run_episode())
+    if mpi_util.rank == 0:
+        now = datetime.utcnow().strftime("%b-%d_%H:%M:%S")  # create unique directories
+        #aigym_path = os.path.join('/tmp', env_name, now)
+        #env = wrappers.Monitor(env, aigym_path, force=True)
+        logger = Logger(logname=env_name, now=now)
+
+    policy = Policy(obs_dim, act_dim, kl_targ)
+    scaler = Scaler(obs_dim)
+    val_func = NNValueFunction(obs_dim)
+
+    if mpi_util.rank == 0:
+        # run a few episodes (on node 0) of untrained policy to initialize scaler:
+        trajectories = run_policy(env, policy, scaler, episodes=5)
+
+        unscaled = np.concatenate([t['unscaled_obs'] for t in trajectories])
+        scaler.update(unscaled)  # update running statistics for scaling observations
+
+    # broadcast policy weights, scaler, val_func
+    (policy, scaler, val_func) = mpi_util.broadcast_policy_scaler_val(policy, scaler, val_func)
+
+    worker_batch_size = int(batch_size / mpi_util.nworkers) # HACK
+    if (worker_batch_size*mpi_util.nworkers != batch_size):
+        print("batch_size:", batch_size, " is not divisible by nworkers:", mpi_util.nworkers)
+        exit(1)
+
+    episode = 0
+    while episode < num_episodes:
+        trajectories = run_policy(env, policy, scaler, episodes=worker_batch_size)
+        trajectories = mpi_util.gather_trajectories(trajectories)
+
+        if mpi_util.rank == 0:
+            # concatentate trajectories into one list
+            trajectories = list(itertools.chain.from_iterable(trajectories))
+            print("did a batch of ",len(trajectories), " trajectories")
+            print([t['rewards'].sum() for t in trajectories])
+
+            episode += len(trajectories)
+            add_value(trajectories, val_func)  # add estimated values to episodes
+            add_disc_sum_rew(trajectories, gamma)  # calculated discounted sum of Rs
+            add_gae(trajectories, gamma, lam)  # calculate advantage
+
+            # concatenate all episodes into single NumPy arrays
+            observes, actions, advantages, disc_sum_rew = build_train_set(trajectories)
+
+            # add various stats to training log:
+            logger.log({'_MeanReward': np.mean([t['rewards'].sum() for t in trajectories]),
+                        'Steps': np.sum([t['observes'].shape[0] for t in trajectories])})
+            log_batch_stats(observes, actions, advantages, disc_sum_rew, logger, episode)
+
+            policy.update(observes, actions, advantages, logger)  # update policy
+            val_func.fit(observes, disc_sum_rew, logger)  # update value function
+
+            unscaled = np.concatenate([t['unscaled_obs'] for t in trajectories])
+            scaler.update(unscaled)  # update running statistics for scaling observations
+
+            logger.write(display=True)  # write logger results to file and stdout
+
+        # if mpi_util.rank == 0 and killer.kill_now:
+        #     if input('Terminate training (y/[n])? ') == 'y':
+        #         break
+        #     killer.kill_now = False
+
+        # broadcast policy weights, scaler, val_func
+        (policy, scaler, val_func) = mpi_util.broadcast_policy_scaler_val(policy, scaler, val_func)
+
+    if mpi_util.rank == 0: logger.close()
+    policy.close_sess()
+    if mpi_util.rank == 0: val_func.close_sess()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=('Train policy on OpenAI Gym environment '
+                                                  'using Proximal Policy Optimizer'))
+    parser.add_argument('env_name', type=str, help='OpenAI Gym environment name')
+    parser.add_argument('-n', '--num_episodes', type=int, help='Number of episodes to run',
+                        default=1000000000000)
+    parser.add_argument('-g', '--gamma', type=float, help='Discount factor', default=0.995)
+    parser.add_argument('-l', '--lam', type=float, help='Lambda for Generalized Advantage Estimation',
+                        default=0.98)
+    parser.add_argument('-k', '--kl_targ', type=float, help='D_KL target value',
+                        default=0.003)
+    parser.add_argument('-b', '--batch_size', type=int,
+                        help='Number of episodes per training batch',
+                        default=20)
+
+    parser.add_argument('--nprocs', type=int, default=1)
+    parser.add_argument('--gpu_pct', type=float, default=0.0, help ='tensorflow  per_process_gpu_memory_fraction  option. .08 may work for 10 processes')
+    parser.add_argument('--policy_hid_list', type=str, help='comma separated 3 layer list.  [30,40,25]', default='[]')
+    parser.add_argument('--valfunc_hid_list', type=str, help='comma separated 3 layer list.  [30,40,25]', default='[]')
+
+    args = parser.parse_args()
+    args.policy_hid_list = eval(args.policy_hid_list)
+    args.valfunc_hid_list = eval(args.valfunc_hid_list)
+    if "parent" == mpi_util.mpi_fork(args.nprocs, gpu_pct=args.gpu_pct): sys.exit()
+    main(**vars(args))
